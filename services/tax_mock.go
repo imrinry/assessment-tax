df47712eddf2a6116cf19266790944019b112732
Jarin Khae-im <imrinry@gmail.com>
2024-04-26 22:51:31 +0700

    add some test case

---
+++ services/tax_mock.go
@@ -0,0 +1,19 @@
+package services
+
+// import (
+// 	"github.com/imrinry/assessment-tax/models"
+// 	"github.com/stretchr/testify/mock"
+// )
+
+// type taxServiceMock struct {
+// 	mock.Mock
+// }
+
+// func NewTaxServiceMock() *taxServiceMock {
+// 	return &taxServiceMock{}
+// }
+
+// func (m *taxServiceMock) TaxCalculations() (models.TaxResponse, error) {
+// 	args := m.Called()
+// 	return args.Get(0).(models.TaxResponse), args.Error(1)
+// }
